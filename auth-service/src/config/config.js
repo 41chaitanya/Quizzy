a60bb5b0f2c696589c217d8e0cd458b53bd81b34
Kaif1119 <mohdkaifsiddiqui1196@gmail.com>
2026-05-14 23:00:01 +0530

    refactor: Simplify environment variable validation logic

--- auth-service/src/config/config.js
+++ auth-service/src/config/config.js
@@ -3,29 +3,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Validate required environment variables before application startup
-if (!process.env.PORT) {
-  console.error("PORT is not defined in environment variables");
-  process.exit(1);
-}
 
-if (!process.env.MONGO_URI) {
-  console.error("MONGO_URI is not defined in environment variables");
-  process.exit(1);
-}
+const requireENV = [
+  "PORT",
+  "MONGO_URI",
+  "ACCESS_TOKEN_SECRET",
+  "ACCESS_TOKEN_EXPIRE",
+  "REFRESH_TOKEN_SECRET",
+  "REFRESH_TOKEN_EXPIRE",
+];
 
-if (!process.env.ACCESS_TOKEN_SECRET || !process.env.ACCESS_TOKEN_EXPIRE) {
-  console.error(
-    "ACCESS_TOKEN_SECRET and ACCESS_TOKEN_EXPIRE are not defined in environment variables",
-  );
-  process.exit(1);
-}
+requireENV.forEach((key) => {
+  if (!process.env[key]) {
+    console.log(`${key} is not defined in environment variables`);
+    process.exit(1);
+  }
+});
 
-if (!process.env.REFRESH_TOKEN_SECRET || !process.env.REFRESH_TOKEN_EXPIRE) {
-  console.error(
-    "REFRESH_TOKEN_SECRET and REFRESH_TOKEN_EXPIRE are not defined in environment variables",
-  );
-  process.exit(1);
-}
 
 const config = {
   PORT: process.env.PORT,

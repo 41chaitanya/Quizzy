a89fe8d773bd1df6598ca1aeff626b6d05638187
shubdev <shub.mahadule@gmail.com>
2026-05-15 01:10:40 +0530

    add controller middleware and complete authentication

---
+++ code _flow.js
@@ -0,0 +1,39 @@
+/**
+ * CODE FLOW
+ * 
+ * ---API  FLOW---
+ * Routes
+   ↓
+Controllers
+   ↓
+Services
+   ↓
+DAO
+   ↓
+MongoDB Model
+   ↓
+Database
+
+
+---Authentication FLOW---
+Login
+  ↓
+Generate JWT
+  ↓
+Send Token
+  ↓
+Client Stores Token
+  ↓
+Authorization Header
+  ↓
+Middleware Verifies Token
+  ↓
+Protected Route Access
+
+
+
+
+
+
+
+ */
